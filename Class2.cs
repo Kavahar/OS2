fc5fbec647e41bfd181ae5e9b68a0e489cca815c
vladi <vladi@LAPTOP-ST2K74IK>
2022-04-07 10:52:44 +0300

    Добавьте файлы проекта.

---
+++ Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace Laba2_OS
+{
+    class Consumer
+    {
+        private ChannelReader<string> Reader;
+        private string PasswordHash;
+
+        public Consumer(ChannelReader<string> _reader, string _passwordHash)
+        {
+            Reader = _reader;
+            PasswordHash = _passwordHash;
+            Task.WaitAll(Run());
+        }
+
+        private async Task Run()
+        {
+            while (await Reader.WaitToReadAsync())
+            {
+                if (!Program.foundFlag)
+                {
+                    var item = await Reader.ReadAsync();
+                    if (FoundHash(item.ToString()) == PasswordHash)
+                    {
+                        Console.WriteLine($"\tПароль подобран - {item}");
+                        Program.foundFlag = true;
+                    }
+                }
+                else return;
+            }
+        }
+        /// <summary>
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        static public string FoundHash(string str)
+        {
+            SHA256 sha256Hash = SHA256.Create();
+            byte[] sourceBytes = Encoding.ASCII.GetBytes(str);
+            byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+            string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+            return hash;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading.Channels;
+using Laba2_OS;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        const string PATH = "passwordHashes.txt";
+        static public bool foundFlag = false;
+
+        static void printMenu()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. Выполнение.");
+                Console.WriteLine("2. Очистить консоль.");
+                Console.WriteLine("3. Exit");
+                Console.Write("Выберите пункт меню: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\tВыберите по какому хеш значению подобрать пароль: ");
+                        Console.WriteLine("\t1.");
+                        Console.WriteLine("\t2.");
+                        Console.WriteLine("\t3.");
+                        Console.Write("\t---> ");
+                        int sign = int.Parse(Console.ReadLine());
+                        string[] readText = File.ReadAllLines(PATH);
+                        string passwordHash = readText[sign - 1].ToUpper();
+                        Console.Write("\tВведите количество потоков: ");
+                        int countStream = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\t...");
+
+                        Channel<string> channel = Channel.CreateBounded<string>(countStream);
+                        Stopwatch time = new();
+                        time.Reset();
+                        time.Start();
+                        var prod = Task.Run(() => { new Producer(channel.Writer); });
+                        Task[] streams = new Task[countStream + 1];
+                        streams[0] = prod;
+                        for (int i = 1; i < countStream + 1; i++)
+                        {
+                            streams[i] = Task.Run(() => { new Consumer(channel.Reader, passwordHash); });
+                        }
+                        Task.WaitAny(streams);
+                        time.Stop();
+                        Console.WriteLine($"\tЗатраченное время на подбор: {time.Elapsed}");
+                        Console.WriteLine("\tВведите ENTER, чтобы выйти в главное меню.");
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        foundFlag = false;
+                        break;
+                    case 2:
+                        Console.Clear();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("\tВыбранного пункта нет в меню.");
+                        break;
+                }
+            }
+        }
+
+        static public void Main()
+        {
+            printMenu();
+        }
+    }
+}
